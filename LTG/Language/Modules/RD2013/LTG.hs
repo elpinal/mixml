53fd0efc7ad2d97684d63f87847091a73e189296
elpinal <elpinal@users.noreply.github.com>
2019-05-08 19:39:17 +0900

    Add Term

--- LTG/Language/Modules/RD2013/LTG.hs
+++ LTG/Language/Modules/RD2013/LTG.hs
@@ -28,6 +28,9 @@ module Language.Modules.RD2013.LTG
   , Type(..)
   , MType
 
+  -- * Terms
+  , Term(..)
+
   -- * Pretty-printing
   , Prec(..)
   , Named(..)
@@ -36,7 +39,10 @@ module Language.Modules.RD2013.LTG
 import Control.Monad
 import Control.Monad.Freer
 import Control.Monad.Freer.Reader
+import Control.Monad.Freer.State
 import qualified Data.Map.Strict as Map
+import Data.Monoid
+import Data.Semigroup
 import Data.String
 import qualified Data.Text as T
 import Data.Text.Prettyprint.Doc
@@ -86,7 +92,7 @@ instance IsList (Record a) where
 
 instance PrettyEnv a => PrettyEnv (Record a) where
   prettyEnv _ (Record m) = do
-    m <- mapM (prettyEnv 0) m
+    m <- mapM prettyEnv0 m
     let x = hcat $ punctuate (semi <> line) $ (\(l, d) -> hsep [pretty l, "=", d]) <$> Map.toList m
     return $ group $ braces $ flatAlt space "" <> align x <> flatAlt (semi <> line) ""
 
@@ -111,6 +117,9 @@ parensWhen False = id
 instance PrettyPrec Kind where
   prettyPrec _ Type         = "*"
   prettyPrec n (KFun k1 k2) = parensWhen (n >= 4) $ prettyPrec 4 k1 <+> "->" <+> prettyPrec 3 k2
+
+instance PrettyEnv Kind where
+  prettyEnv n k = return $ prettyPrec n k
 
 type MKind = Moded Kind
 
@@ -145,11 +154,14 @@ incTypeDepth nenv = nenv { tdepth = tdepth nenv + 1 }
 class PrettyEnv a where
   prettyEnv :: Member (Reader NameEnv) r => Int -> a -> Eff r (Doc ann)
 
+prettyEnv0 :: (PrettyEnv a, Member (Reader NameEnv) r) => a -> Eff r (Doc ann)
+prettyEnv0 = prettyEnv 0
+
 newtype Named a = Named a
   deriving (Eq, Show)
 
 instance PrettyEnv a => Pretty (Named a) where
-  pretty (Named x) = run $ runReader nameEnv $ prettyEnv 0 x
+  pretty (Named x) = run $ runReader nameEnv $ prettyEnv0 x
 
 prettyTypeVariable :: Member (Reader NameEnv) r => Variable -> Eff r (Doc ann)
 prettyTypeVariable (Global n)   = return $ "g" <> pretty n
@@ -174,9 +186,21 @@ data Binder
   | Bind Int
   deriving (Eq, Show)
 
+isIndex :: Binder -> Bool
+isIndex Index    = True
+isIndex (Bind _) = False
+
+ifIndex :: Binder -> (a -> a) -> a -> a
+ifIndex Index f    = f
+ifIndex (Bind _) _ = id
+
 prettyTypeBinder :: Member (Reader NameEnv) r => Binder -> Eff r (Doc ann)
 prettyTypeBinder (Bind n) = prettyTypeVariable (Global n)
 prettyTypeBinder Index    = local incTypeDepth $ prettyTypeVariable $ Variable 0
+
+prettyBinder :: Member (Reader NameEnv) r => Binder -> Eff r (Doc ann)
+prettyBinder (Bind n) = prettyVariable (Global n)
+prettyBinder Index    = local incValueDepth $ prettyVariable $ Variable 0
 
 data Type
   = TFun MType MType
@@ -189,22 +213,69 @@ data Type
   | TApp Type Type
   deriving (Eq, Show)
 
-prettyBind :: (Member (Reader NameEnv) r, PrettyPrec k, PrettyEnv ty) => Int -> Binder -> k -> ty -> Doc ann -> Eff r (Doc ann)
-prettyBind n b k ty d = fmap (parensWhen $ n >= 4) $ f <$> prettyTypeBinder b <*> local incTypeDepth (prettyEnv 0 ty)
+prettyTypeBind :: (Member (Reader NameEnv) r, PrettyPrec k, PrettyEnv ty) => Int -> Binder -> k -> ty -> Doc ann -> Eff r (Doc ann)
+prettyTypeBind n b k ty d = fmap (parensWhen $ n >= 4) $ f <$> prettyTypeBinder b <*> local (ifIndex b incTypeDepth) (prettyEnv0 ty)
   where f x y = d <> x <+> ":" <+> pretty (Prec k) <> "." <+> y
 
 instance PrettyEnv Type where
   prettyEnv n (TFun ty1 ty2) = fmap (parensWhen $ n >= 4) $ f <$> prettyEnv 4 ty1 <*> prettyEnv 3 ty2
     where f d1 d2 = hsep [d1, "->", d2]
   prettyEnv n (TRecord r)     = prettyEnv n r
-  prettyEnv n (Forall b k ty) = prettyBind n b k ty "∀"
-  prettyEnv n (Some b k ty)   = prettyBind n b k ty "∃"
+  prettyEnv n (Forall b k ty) = prettyTypeBind n b k ty "∀"
+  prettyEnv n (Some b k ty)   = prettyTypeBind n b k ty "∃"
   prettyEnv n (Ref ty)        = parensWhen (n >= 9) <$> liftM2 (<>) (return "?") (prettyEnv 9 ty)
   prettyEnv _ (TVar v)        = prettyTypeVariable v
-  prettyEnv n (TAbs b k ty)   = prettyBind n b k ty "λ"
+  prettyEnv n (TAbs b k ty)   = prettyTypeBind n b k ty "λ"
   prettyEnv n (TApp ty1 ty2)  = fmap (parensWhen $ n >= 5) $ liftM2 (<+>) (prettyEnv 4 ty1) (prettyEnv 5 ty2)
 
 type MType = Moded Type
+
+data Term
+  = Var Variable
+  | Abs Binder MType Term
+  | App Term Term
+  | TmRecord (Record Term)
+  | Let (Record Binder) Term Term
+  | New Type
+  | Def Term Term
+  | Read Term
+  | Poly Binder MKind Term
+  | Inst Term Type
+  | Pack Type Term Type
+  | Unpack Binder Binder Term Term
+  | NewIn Binder Kind Term
+  | DefIn Type Type Term MType
+  deriving (Eq, Show)
+
+prettyBind :: (Member (Reader NameEnv) r, PrettyEnv k, PrettyEnv ty) => Int -> Binder -> k -> ty -> Doc ann -> Eff r (Doc ann)
+prettyBind n b k ty d = fmap (parensWhen $ n >= 4) $ f <$> prettyBinder b <*> prettyEnv0 k <*> local (ifIndex b incValueDepth) (prettyEnv0 ty)
+  where f x y z = d <> x <+> ":" <+> y <> "." <+> z
+
+toVariable :: Member (State Int) r => Binder -> Eff r Variable
+toVariable Index    = modify (subtract (1 :: Int)) >> gets variable
+toVariable (Bind n) = return $ global n
+
+instance PrettyEnv Term where
+  prettyEnv _ (Var v)       = prettyVariable v
+  prettyEnv n (Abs b ty t)  = prettyBind n b ty t "λ"
+  prettyEnv n (App t1 t2)   = fmap (parensWhen $ n >= 5) $ liftM2 (<+>) (prettyEnv 4 t1) (prettyEnv 5 t2)
+  prettyEnv n (TmRecord r)  = prettyEnv n r
+  prettyEnv n (Let r t1 t2) = do
+    let nn = length . filter isIndex $ snd <$> toList r
+    let f (l, b) = local (appEndo $ mtimesDefault nn $ Endo incValueDepth) $ toVariable b >>= prettyVariable >>= \x -> return (hsep [pretty l, "=", x])
+    d <- hsep . punctuate semi <$> evalState nn (sequence $ f <$> toList r)
+    d1 <- prettyEnv0 t1
+    d2 <- local (appEndo $ mtimesDefault nn $ Endo incValueDepth) $ prettyEnv0 t2
+    return $ parensWhen (n >= 4) $ hsep ["let", braces d, "=", d1, "in", d2]
+  prettyEnv n (New ty)              = parensWhen (n >= 4) . ("new" <+>) <$> prettyEnv 9 ty
+  prettyEnv n (Def t1 t2)           = (\x y -> parensWhen (n >= 4) $ hsep ["def", x, ":=", y]) <$> prettyEnv0 t1 <*> prettyEnv0 t2
+  prettyEnv n (Read t)              = parensWhen (n >= 9) . ("!" <>) <$> prettyEnv 9 t
+  prettyEnv n (Poly b k t)          = prettyTypeBind n b k t "Λ"
+  prettyEnv n (Inst t ty)           = fmap (parensWhen $ n >= 5) $ liftM2 (<+>) (prettyEnv 4 t) (brackets <$> prettyEnv0 ty)
+  prettyEnv n (Pack ty1 t ty2)      = fmap (parensWhen $ n >= 4) $ (\x y z -> hsep ["pack", angles $ x <> comma <+> y, "as", z]) <$> prettyEnv0 ty1 <*> prettyEnv0 t <*> prettyEnv0 ty2
+  prettyEnv n (Unpack b1 b2 t1 t2)  = fmap (parensWhen $ n >= 4) $ (\w x y z -> hsep ["unpack", angles $ w <> comma <+> x, "=", y, "in", z]) <$> prettyTypeBinder b1 <*> prettyBinder b2 <*> prettyEnv0 t1 <*> local (ifIndex b2 incValueDepth . ifIndex b1 incTypeDepth) (prettyEnv0 t2)
+  prettyEnv n (NewIn b k t)         = fmap (parensWhen $ n >= 4) $ (\x y z -> hsep ["new", x, ":", y, "in", z]) <$> prettyTypeBinder b <*> prettyEnv0 k <*> local (ifIndex b incTypeDepth) (prettyEnv0 t)
+  prettyEnv n (DefIn ty1 ty2 t ty3) = fmap (parensWhen $ n >= 4) $ (\w x y z -> hsep ["def", w, ":=", x, "in", y, ":", z]) <$> prettyEnv0 ty1 <*> prettyEnv0 ty2 <*> prettyEnv 9 t <*> prettyEnv0 ty3
 
 data Env = Env
   { tenv :: Map.Map Variable MKind
--- test/Language/Modules/RD2013/LTGSpec.hs
+++ test/Language/Modules/RD2013/LTGSpec.hs
@@ -61,7 +61,8 @@ spec = do
       show (pretty $ Named $ un ty1 +> lin ty1) `shouldBe` "(∀t0 : *. t0) -> (∀t0 : *. t0)@L"
 
       show (pretty $ Named $ Some Index (un Type) $ un v3) `shouldBe` "∃t0 : *. t0"
-      show (pretty $ Named $ TAbs Index Type $ v3)         `shouldBe` "λt0 : *. t0"
+      show (pretty $ Named $ TAbs Index Type v3)           `shouldBe` "λt0 : *. t0"
+      show (pretty $ Named $ TAbs (Bind 8) Type v3)        `shouldBe` "λg8 : *. v[0]"
 
       show (pretty $ Named $ Ref v1)                      `shouldBe` "?g3"
       show (pretty $ Named $ Ref $ Ref v1)                `shouldBe` "?(?g3)"
@@ -75,3 +76,93 @@ spec = do
       show (pretty $ Named $ TApp v1 $ v2 `TApp` v1)               `shouldBe` "g3 (g47 g3)"
       show (pretty $ Named $ un (TApp v1 v2) +> un (TApp v2 v1))   `shouldBe` "g3 g47 -> g47 g3"
       show (pretty $ Named $ lin (TApp v1 v2) +> lin (TApp v2 v1)) `shouldBe` "(g3 g47)@L -> (g47 g3)@L"
+
+    it "pretty-prints terms" $ do
+      show (pretty $ Named $ Var $ variable 80) `shouldBe` "v[80]"
+      show (pretty $ Named $ Var $ global 79)   `shouldBe` "g79"
+
+      let v = un $ TVar $ global 41
+
+      show (pretty $ Named $ Abs Index v $ Var $ global 79)                 `shouldBe` "λv0 : g41. g79"
+      show (pretty $ Named $ Abs Index (un $ v +> v) $ Var $ global 79)     `shouldBe` "λv0 : g41 -> g41. g79"
+      show (pretty $ Named $ Abs Index (lin $ v +> v) $ Var $ global 79)    `shouldBe` "λv0 : (g41 -> g41)@L. g79"
+      show (pretty $ Named $ Abs Index (un $ v +> v) $ Var $ variable 0)    `shouldBe` "λv0 : g41 -> g41. v0"
+      show (pretty $ Named $ Abs Index (un $ v +> v) $ Var $ variable 1)    `shouldBe` "λv0 : g41 -> g41. v[1]"
+      show (pretty $ Named $ Abs (Bind 8) (un $ v +> v) $ Var $ variable 0) `shouldBe` "λg8 : g41 -> g41. v[0]"
+
+      let t = Var $ global 3
+
+      show (pretty $ Named $ App t t)               `shouldBe` "g3 g3"
+      show (pretty $ Named $ App t $ App t t)       `shouldBe` "g3 (g3 g3)"
+      show (pretty $ Named $ App (App t t) t)       `shouldBe` "g3 g3 g3"
+      show (pretty $ Named $ App (Abs Index v t) t) `shouldBe` "(λv0 : g41. g3) g3"
+      show (pretty $ Named $ App t (Abs Index v t)) `shouldBe` "g3 (λv0 : g41. g3)"
+
+      show (pretty $ Named $ Let [] (Var $ global 1) (Var $ global 33))                              `shouldBe` "let {} = g1 in g33"
+      show (pretty $ Named $ Let [("xy", Bind 20)] (Var $ global 1) (Var $ global 33))               `shouldBe` "let {xy = g20} = g1 in g33"
+      show (pretty $ Named $ Let [("xy", Bind 20), ("z", Index)] (Var $ global 5) (Var $ global 39)) `shouldBe` "let {xy = g20; z = v0} = g5 in g39"
+
+      show (pretty $ Named $ App t $ Let [] (Var $ global 1) (Var $ global 33))      `shouldBe` "g3 (let {} = g1 in g33)"
+      show (pretty $ Named $ flip App t $ Let [] (Var $ global 1) (Var $ global 33)) `shouldBe` "(let {} = g1 in g33) g3"
+
+      show (pretty $ Named $ Let [] (Var $ global 1) $ Let [] (Var $ global 5) $ Var $ global 33)  `shouldBe` "let {} = g1 in let {} = g5 in g33"
+      show (pretty $ Named $ Let [] (Let [] (Var $ global 5) $ Var $ global 33) $ Var $ global 40) `shouldBe` "let {} = let {} = g5 in g33 in g40"
+
+      show (pretty $ Named $ Let [("xy", Bind 20)] (Var $ global 1) $ Var $ variable 0)                              `shouldBe` "let {xy = g20} = g1 in v[0]"
+      show (pretty $ Named $ Let [("xy", Index)] (Var $ global 1) $ Var $ variable 0)                                `shouldBe` "let {xy = v0} = g1 in v0"
+      show (pretty $ Named $ Let [("xy", Index), ("xyz", Index)] (Var $ global 1) $ Var $ variable 0)                `shouldBe` "let {xy = v0; xyz = v1} = g1 in v1"
+      show (pretty $ Named $ Let [("xy", Index), ("xyz", Bind 5), ("B", Index)] (Var $ global 1) $ Var $ variable 1) `shouldBe` "let {B = v0; xy = v1; xyz = g5} = g1 in v0"
+      show (pretty $ Named $ Let [("xy", Index), ("xyz", Bind 5), ("z", Index)] (Var $ global 1) $ Var $ variable 1) `shouldBe` "let {xy = v0; xyz = g5; z = v1} = g1 in v0"
+
+      show (pretty $ Named $ New $ TVar $ global 6)               `shouldBe` "new g6"
+      show (pretty $ Named $ App t $ New $ TVar $ global 6)       `shouldBe` "g3 (new g6)"
+      show (pretty $ Named $ App (New $ TVar $ global 6) t)       `shouldBe` "(new g6) g3"
+      show (pretty $ Named $ Abs Index v $ New $ TVar $ global 6) `shouldBe` "λv0 : g41. new g6"
+
+      show (pretty $ Named $ Def t t)               `shouldBe` "def g3 := g3"
+      show (pretty $ Named $ Def t $ Abs Index v t) `shouldBe` "def g3 := λv0 : g41. g3"
+
+      let t1 = Var $ global 20
+
+      show (pretty $ Named $ Read t)                 `shouldBe` "!g3"
+      show (pretty $ Named $ Read $ Read t)          `shouldBe` "!(!g3)"
+      show (pretty $ Named $ App (Read t1) (Read t)) `shouldBe` "!g20 !g3"
+      show (pretty $ Named $ Read $ App t t1)        `shouldBe` "!(g3 g20)"
+
+      let k = un Type
+
+      show (pretty $ Named $ Poly Index k t)                                      `shouldBe` "Λt0 : *. g3"
+      show (pretty $ Named $ Abs Index (un $ TVar $ variable 0) t)                `shouldBe` "λv0 : v[0]. g3"
+      show (pretty $ Named $ Poly Index k $ Abs Index (un $ TVar $ variable 0) t) `shouldBe` "Λt0 : *. λv0 : t0. g3"
+      show (pretty $ Named $ Read $ Poly Index k t)                               `shouldBe` "!(Λt0 : *. g3)"
+
+      show (pretty $ Named $ Inst t $ TVar $ global 9)                           `shouldBe` "g3 [g9]"
+      show (pretty $ Named $ App t $ Inst t $ TVar $ global 9)                   `shouldBe` "g3 (g3 [g9])"
+      show (pretty $ Named $ flip App t $ Inst t $ TVar $ global 9)              `shouldBe` "g3 [g9] g3"
+      show (pretty $ Named $ Inst (Inst t $ TVar $ global 9) $ TVar $ global 10) `shouldBe` "g3 [g9] [g10]"
+      show (pretty $ Named $ Inst (Inst t $ TVar $ global 9) $ v +> v)           `shouldBe` "g3 [g9] [g41 -> g41]"
+
+      show (pretty $ Named $ Pack (TVar $ global 15) t $ TVar $ global 7)              `shouldBe` "pack <g15, g3> as g7"
+      show (pretty $ Named $ App t $ Pack (TVar $ global 15) t $ TVar $ global 7)      `shouldBe` "g3 (pack <g15, g3> as g7)"
+      show (pretty $ Named $ flip App t $ Pack (TVar $ global 15) t $ TVar $ global 7) `shouldBe` "(pack <g15, g3> as g7) g3"
+      show (pretty $ Named $ Def t $ Pack (TVar $ global 15) t $ TVar $ global 7)      `shouldBe` "def g3 := pack <g15, g3> as g7"
+
+      let v = Var $ variable 0
+      let t2 = Abs Index (un $ TVar $ variable 0) v
+
+      show (pretty $ Named $ Unpack Index Index t t1)  `shouldBe` "unpack <t0, v0> = g3 in g20"
+      show (pretty $ Named $ Unpack Index Index v v)   `shouldBe` "unpack <t0, v0> = v[0] in v0"
+      show (pretty $ Named $ Unpack Index Index t2 t2) `shouldBe` "unpack <t0, v0> = λv0 : v[0]. v0 in λv1 : t0. v1"
+
+      show (pretty $ Named $ Unpack Index (Bind 7) t2 t2)    `shouldBe` "unpack <t0, g7> = λv0 : v[0]. v0 in λv0 : t0. v0"
+      show (pretty $ Named $ Unpack (Bind 4) (Bind 7) t2 t2) `shouldBe` "unpack <g4, g7> = λv0 : v[0]. v0 in λv0 : v[0]. v0"
+
+      show (pretty $ Named $ NewIn Index Type v)  `shouldBe` "new t0 : * in v[0]"
+      show (pretty $ Named $ NewIn Index Type t2) `shouldBe` "new t0 : * in λv0 : t0. v0"
+      show (pretty $ Named $ App t $ NewIn Index Type t2) `shouldBe` "g3 (new t0 : * in λv0 : t0. v0)"
+      show (pretty $ Named $ flip App t $ NewIn Index Type t2) `shouldBe` "(new t0 : * in λv0 : t0. v0) g3"
+
+      let ty1 = TVar $ global 27
+      let ty2 = TVar $ global 40
+
+      show (pretty $ Named $ DefIn ty1 ty2 t2 $ un ty1) `shouldBe` "def g27 := g40 in (λv0 : v[0]. v0) : g27"
